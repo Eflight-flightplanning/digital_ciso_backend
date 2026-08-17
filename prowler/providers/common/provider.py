a260b5226c669c92927a58cd29071c27f077c125
Jasaero8555 <githubjas@eflight.aero>
2026-08-17 13:22:27 +0530

    fix(prowler): implement set_global_provider and get_global_provider on Provider base class and clean scan dispatch

--- prowler/providers/common/provider.py
+++ prowler/providers/common/provider.py
@@ -1,9 +1,30 @@
 providers_path = "prowler.providers"
 
 class Provider:
+    _global_provider = None
+
+    @classmethod
+    def set_global_provider(cls, provider):
+        cls._global_provider = provider
+
+    @classmethod
+    def get_global_provider(cls):
+        return cls._global_provider
+
     @staticmethod
     def get_available_providers():
-        return ["aws", "azure", "gcp", "kubernetes", "github", "m365"]
+        return [
+            "aws",
+            "azure",
+            "gcp",
+            "kubernetes",
+            "github",
+            "m365",
+            "oraclecloud",
+            "alibabacloud",
+            "cloudflare",
+            "okta",
+        ]
 
     @staticmethod
     def init_provider(provider_type, **kwargs):
